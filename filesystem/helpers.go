8283d4b9a6db3d44eae88fd3cc58887a5d8c42e9
so0k <vincent.drl@gmail.com>
2021-07-07 10:30:26 +0700

    Replace os.Rename() to avoid cross-link error

--- filesystem/helpers.go
+++ filesystem/helpers.go
@@ -17,6 +17,7 @@ package filesystem
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -165,7 +166,7 @@ func atomicWrite(i *atomicWriteInput) error {
 		}
 	}
 
-	if err := os.Rename(f.Name(), i.dest); err != nil {
+	if err := os.MoveFile(f.Name(), i.dest); err != nil {
 		return errors.Wrap(err, "failed to rename")
 	}
 
@@ -174,6 +175,30 @@ func atomicWrite(i *atomicWriteInput) error {
 	}
 
 	return nil
+}
+
+func MoveFile(sourcePath, destPath string) error {
+    inputFile, err := os.Open(sourcePath)
+    if err != nil {
+        return fmt.Errorf("Couldn't open source file: %s", err)
+    }
+    outputFile, err := os.Create(destPath)
+    if err != nil {
+        inputFile.Close()
+        return fmt.Errorf("Couldn't open dest file: %s", err)
+    }
+    defer outputFile.Close()
+    _, err = io.Copy(outputFile, inputFile)
+    inputFile.Close()
+    if err != nil {
+        return fmt.Errorf("Writing to output file failed: %s", err)
+    }
+    // The copy was successful, so now delete the original file
+    err = os.Remove(sourcePath)
+    if err != nil {
+        return fmt.Errorf("Failed removing original file: %s", err)
+    }
+    return nil
 }
 
 func parseFileMode(s string) (os.FileMode, error) {
